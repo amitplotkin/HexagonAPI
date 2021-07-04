5df0321e46c5f2fcfea374bb761f300858df8870
Amit Plotkin <a_plotkin@rambam.health.gov.il>
2021-07-04 12:29:05 +0300

    docker pull amitplotkin/hexagonapi:v1
    docker pull amitplotkin/hexagonapi:v1

--- DockerBuild.ps1
+++ DockerBuild.ps1
@@ -6,3 +6,6 @@ docker tun -it hexagonapi:v1
 docker login --username amitplotkin
 docker tag hexagonapi:v1 amitplotkin/hexagonapi:v1
 docker push amitplotkin/hexagonapi:v1
+
+#this is the image on docker hub
+docker pull amitplotkin/hexagonapi:v1
